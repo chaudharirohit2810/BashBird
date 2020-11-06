d71b42b7efc211cc5f82f32af4560bb67b48d439
Rohit Chaudhari <rohit@pop-os.localdomain>
2020-11-06 16:07:55 +0530

    fix: Solved clicking on logout exiting out of app issue

--- main_menu.py
+++ main_menu.py
@@ -18,7 +18,7 @@ class Main_Menu:
     '''
 
     # <!---------------------------------------------------Variables------------------------------------------------>
-    __menu_strings = ["Exit"]
+    __menu_strings = ["Logout", "Exit"]
     __menu = []
     __stdscr = None
     # Confirm Email Variables
@@ -32,65 +32,9 @@ class Main_Menu:
         menu = [{'title': "Write mail", 'Function': Write_Mail_UI, 'args': None}]
         menu.append(
             {'title': "View mails", 'Function': Show_Folders, 'args': None})
-        menu.append(
-            {'title': "Logout", 'Function': self.__set_confirm_email_bar, 'args': "STDSCR_NR"})
         for item in self.__menu_strings:
-            # Alert: Function will expect first arguement as stdscr for sure
             menu.append({'title': item, 'Function': None, 'args': None})
         self.__menu = menu
-
-    def __display_bottom_bar_menu(self):
-        '''To display UI of confirm email bottom bar for logout'''
-
-        h, _ = self.__stdscr.getmaxyx()
-
-        start_h = h - 3
-        for index, item in enumerate(self.__confirm_menu):
-
-            y_pos = start_h + index
-            # Check if index is of currently selected item if yes make its background white
-            if self.__curr_confirm_index == index:
-                self.__stdscr.attron(curses.color_pair(1))
-
-            # Print string on screen
-            self.__stdscr.addstr(y_pos, 2, item)
-
-            if self.__curr_confirm_index == index:
-                self.__stdscr.attroff(curses.color_pair(1))
-
-        self.__stdscr.refresh()
-
-    def __set_confirm_email_bar(self):
-        '''Setup confirm email bar for logout'''
-
-        h, w = self.__stdscr.getmaxyx()
-        title = " Do you want to logout ".upper()
-        rectangle(self.__stdscr, h - 4, 0, h - 1, w - 2)
-        self.__stdscr.attron(curses.A_BOLD)
-        self.__stdscr.addstr(h - 4, 1, title)
-        self.__stdscr.attroff(curses.A_BOLD)
-        self.__display_bottom_bar_menu()
-
-        while 1:
-            key = self.__stdscr.getch()
-
-            if key == curses.KEY_UP and self.__curr_confirm_index != 0:
-                self.__curr_confirm_index -= 1
-            elif key == curses.KEY_DOWN and self.__curr_confirm_index != len(self.__confirm_menu) - 1:
-                self.__curr_confirm_index += 1
-
-            # TODO: Do the functionality according to choice of user
-            elif key == curses.KEY_ENTER or key in [10, 13]:
-                if self.__curr_confirm_index == 0:
-                    # Remove the .env file
-                    # Get the environment filepath
-                    user = getpass.getuser()
-                    cred = Credentials()
-                    cred.remote_credentials()
-                    sys.exit()
-                break
-
-            self.__display_bottom_bar_menu()
 
     def show(self):
         '''To show the menu'''
--- menu.py
+++ menu.py
@@ -1,9 +1,18 @@
-import time, curses, sys, textwrap
+import time
+import curses
+import sys
+import textwrap
 from write_mail import Write_Mail_UI
 from curses.textpad import rectangle
+from get_credentials import Credentials
+import getpass
+import login
+
 
 '''Temporary function to check if passing function to dictionary is working'''
 # TODO: Remove this later
+
+
 def temp(stdscr):
     # Get height and width of screen
     h, w = stdscr.getmaxyx()
@@ -26,7 +35,6 @@ def temp(stdscr):
         key = stdscr.getch()
 
 
-
 class Menu:
     '''Utility Menu class which displays menu on screen
 
@@ -35,21 +43,24 @@ class Menu:
     menu_options: Options available for menu \t
     '''
 
-
     # <-------------------------------------------------Variables------------------------------------------------->
-    __curr_index = 0 # Current position in menu
-    __stdscr = None # Stdscr variable of curses
-    __menu = [] # Available menu options
+    __curr_index = 0  # Current position in menu
+    __stdscr = None  # Stdscr variable of curses
+    __menu = []  # Available menu options
     __is_main = False
     # TODO: Change this message later
     __screen_size_msg = "Screen size is too small! Please increase screen size"
     __title = ""
 
+    # Confirm Email Variables
+    __curr_confirm_index = 0
+    __confirm_menu = ["YES", "NO"]
 
-    #<---------------------------------------------------Functions------------------------------------------------->
-    def __init__(self, stdscr, menu_options, title, isMain = False):
+    # <---------------------------------------------------Functions------------------------------------------------->
+
+    def __init__(self, stdscr, menu_options, title, isMain=False):
         curses.curs_set(0)
-        
+
         self.__stdscr = stdscr
         self.__menu = menu_options
         self.__title = " " + title.upper() + " "
@@ -78,21 +89,19 @@ class Menu:
                     # If not go back
                     else:
                         break
+                elif self.__curr_index == len(self.__menu) - 2 and self.__is_main:
+                    self.__set_confirm_email_bar()
+                    break
                 else:
                     self.__on_enter_pressed()
                     continue
-            
+
             self.__display_menu()
 
+    # <-------------------------------------------Private Functions------------------------------------->
 
-
-
-
-    #<-------------------------------------------Private Functions------------------------------------->
-
-       
     def __display_menu(self):
-        '''Main function which displays menu''' 
+        '''Main function which displays menu'''
 
         # Get height and width of screen (Required for centering menu)
         h, w = self.__stdscr.getmaxyx()
@@ -102,7 +111,7 @@ class Menu:
 
         # Clear the screen
         self.__stdscr.clear()
-        
+
         y_start = h // 2 - len(self.__menu) // 2
 
         # Iterate over each option in menu
@@ -149,24 +158,25 @@ class Menu:
             y_start -= 4
         else:
             y_start -= 2
-        
+
         if y_end + 4 < h - 1:
             y_end += 4
         else:
             y_end += 1
-        
+
         try:
             # Show the title of menu and rectangle
             rectangle(self.__stdscr, y_start, x_start, y_end, x_end)
             self.__stdscr.attron(curses.A_BOLD)
-            self.__stdscr.addstr(y_start, w // 2 - len(self.__title) // 2 + 2, self.__title)
+            self.__stdscr.addstr(
+                y_start, w // 2 - len(self.__title) // 2 + 2, self.__title)
             self.__stdscr.attroff(curses.A_BOLD)
 
             # Refresh the screen
             self.__stdscr.refresh()
         except:
-            # If it is not possible to show the menu 
-            # then show the message that it cannot be shown on the screen 
+            # If it is not possible to show the menu
+            # then show the message that it cannot be shown on the screen
             self.__stdscr.clear()
             wrapper = textwrap.TextWrapper(width=w-2)
             error_msgs = wrapper.wrap(self.__screen_size_msg)
@@ -175,20 +185,17 @@ class Menu:
                 y_pos = h // 2 + index
                 self.__stdscr.addstr(y_pos, x_pos, msg)
             self.__stdscr.refresh()
-        
 
-
-
-   
     def __on_enter_pressed(self):
         '''When enter is pressed for particular item'''
-        
+
         if self.__menu[self.__curr_index]['args'] == "STDSCR_NR":
             self.__menu[self.__curr_index]['Function']()
         elif self.__menu[self.__curr_index]['args'] != None:
             arguements = self.__menu[self.__curr_index]['args']
             arg1, arg2 = arguements
-            self.__menu[self.__curr_index]['Function'](self.__stdscr, arg1, arg2)
+            self.__menu[self.__curr_index]['Function'](
+                self.__stdscr, arg1, arg2)
         else:
             # TODO: Also pass arguements to this function
             # Call the desired function
@@ -197,27 +204,77 @@ class Menu:
         # Display menu again after pressing enter
         self.__display_menu()
 
+    # <--------------------------------------------Utility functions---------------------------------------->
 
-
-    #<--------------------------------------------Utility functions---------------------------------------->
-    
-    
     def __setup_color_pairs(self):
         '''Function to setup color pairs required'''
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-        
+    def __display_bottom_bar_menu(self):
+        '''To display UI of confirm email bottom bar for logout'''
 
+        h, _ = self.__stdscr.getmaxyx()
+
+        start_h = h - 3
+        for index, item in enumerate(self.__confirm_menu):
+
+            y_pos = start_h + index
+            # Check if index is of currently selected item if yes make its background white
+            if self.__curr_confirm_index == index:
+                self.__stdscr.attron(curses.color_pair(1))
+
+            # Print string on screen
+            self.__stdscr.addstr(y_pos, 2, item)
+
+            if self.__curr_confirm_index == index:
+                self.__stdscr.attroff(curses.color_pair(1))
+
+        self.__stdscr.refresh()
+
+    def __set_confirm_email_bar(self):
+        '''Setup confirm email bar for logout'''
+
+        h, w = self.__stdscr.getmaxyx()
+        title = " Do you want to logout ".upper()
+        rectangle(self.__stdscr, h - 4, 0, h - 1, w - 2)
+        self.__stdscr.attron(curses.A_BOLD)
+        self.__stdscr.addstr(h - 4, 1, title)
+        self.__stdscr.attroff(curses.A_BOLD)
+        self.__display_bottom_bar_menu()
+
+        while 1:
+            key = self.__stdscr.getch()
+
+            if key == curses.KEY_UP and self.__curr_confirm_index != 0:
+                self.__curr_confirm_index -= 1
+            elif key == curses.KEY_DOWN and self.__curr_confirm_index != len(self.__confirm_menu) - 1:
+                self.__curr_confirm_index += 1
+
+            # TODO: Do the functionality according to choice of user
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                if self.__curr_confirm_index == 0:
+                    # Remove the .env file
+                    # Get the environment filepath
+                    cred = Credentials()
+                    cred.remote_credentials()
+                    # sys.exit()
+                    login.LOGIN_UI(self.__stdscr)
+                break
+
+            self.__display_bottom_bar_menu()
 
 
 menu_strings = ["Show email", "Logout", "Exit"]
+
+
 def main(stdscr):
     menu = [{'title': "Write mail", 'Function': Write_Mail_UI}]
     for item in menu_strings:
         # Alert: Function will expect first arguement as stdscr for sure
         menu.append({'title': item, 'Function': temp})
     Menu(stdscr, menu, "Sample", isMain=True)
+
 
 if __name__ == "__main__":
     curses.wrapper(main)
